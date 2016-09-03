941da3becd17b1e08f7911afc32e6e5d8df4fe34
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-09-03 15:45:24 +0200

    skeleton commands

---
+++ twitter_commands/twitter/management/commands/tweetsreport.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "tweetsreporttt command"
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('poll_id', nargs='+', type=int)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            dest='delete',
+            default=False,
+            help='Delete poll instead of closing it',
+        )
+
+    def handle(self, *args, **options):
+        # ...
+        # ...
+        pass
